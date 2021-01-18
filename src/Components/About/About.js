962f90b8d3aa44dc5e00be6e5d1a7d2aff9298a0
St John lau <stjohnlau@St-Johns-MacBook-Pro.local>
2021-01-17 18:51:58 -0800

    Fixed home and about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,30 @@ import './About.css';
 function About() {
   return (
     <div className="About">
-      <h1>About Page</h1>
+      <h1 className="Title">About Page</h1>
+      <p className="p1">Hi! My name's St John (pronounced like
+         <a href = "https://victorious.fandom.com/wiki/Sinjin_Van_Cleef" target="_blank"> Sinjin from Victorious</a>),
+          and I'm a computer science student at NYU. I've been playing guitar since the beginning of high school,
+          and I decided to make this website as a small project on the side because I found that when I first
+          started learning guitar, all I wanted was a resource that would allow me to find all the chord shapes
+          I wanted to learn that was sorted in a clean and efficient way. So, hopefully this website
+          serves as that tool for you guys! 
+        </p>
+        <p className="p2">This website is always work in progress, and I'll be adding more shapes and 
+        adding other functionalities as time passes. For any inquiries, email sjl792@nyu.edu</p>
+      
+        <p className="p3">I've also released an EP titled <mark className="ep"> Empathizer</mark> that's available on a bunch of different streaming services such as Spotify
+          and Apple Music, so if you guys enjoy the website it'd be amazing if you could check it out!
+        </p>
+
+        <div className="EP-Container">
+          <img className='cover' src="images/EPCover.jpg"></img>
+          <ul className="Links">
+            <li><a className="link" href="https://open.spotify.com/album/6sOhXDp7v1Zgj0OS6fPS65" target="_blank">Spotify</a></li>
+            <li><a className="link" href="https://music.apple.com/us/album/empathizer/1548055492?uo=4&app=music&at=1001lry3&ct=dashboard" target="_blank">Apple Music</a></li>
+            <li><a className="link" href="https://www.amazon.com/gp/product/B08SJ9Z3Z3/?tag=distrokid06-20" target="_blank">Amazon</a></li>
+          </ul>
+        </div>
     </div>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,10 @@ import './Home.css';
 function Home() {
   return (
     <div className="Home">
-      <h1>Home Page</h1>
+      <h1 className="Title">Home Page</h1>
+      <p className = "head1">Welcome! Click on the Chord Shapes button in the top right corner to get started and search for the chord shape you're looking for!</p>
+      <p className ="head2">Below is a chart of all the notes on the fretboard for reference in case you need it</p>
+      <img className="fretboard" src="images/fretboard.png"></img>
     </div>
   );
 }
